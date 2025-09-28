005428fb827d7666ec2ca8e95140d053321719b7
Pinpe <813233375@qq.com>
2025-09-28 23:39:44 +0800

    color

--- src/config.ts
+++ src/config.ts
@@ -11,19 +11,19 @@ export const siteConfig: SiteConfig = {
   subtitle: '一个属于自己的云朵',
   lang: 'zh_CN',         // 语言：'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 290,         // 主题色的默认色调值（0-360）。例如：红色:0, 蓝绿色:200, 青色:250, 粉色:345
+    hue: 60,         // 主题色的默认色调值（0-360）。例如：红色:0, 蓝绿色:200, 青色:250, 粉色:345
     fixed: false,     // 对访客隐藏主题色选择器
   },
   banner: {
     enable: true,    // 是否启用横幅
-    text: '灯火迷楼影，清辉浸夜天', //横幅标题
+    text: '翠柏凝烟深似海，丹枫焕彩灿如霞', //横幅标题
     subText: '你好，我是Pinpe，可能来自于另一个星球的人',  //横幅副标题
     src: 'assets/images/back.jpg',   // 相对于/src目录的路径。若以'/'开头则相对于/public目录
     position: 'center',      // 相当于object-position，仅支持'top'、'center'、'bottom'，默认为'center'
     credit: {
       enable: true,         // 是否显示横幅图片来源信息
-      text: 'cityscape photo',              // 显示的来源文本
-      url: 'https://unsplash.com/photos/cityscape-photo-0UofLbRyr-I'                // （可选）原作品或艺术家页面的链接
+      text: 'Luca Bravo',              // 显示的来源文本
+      url: 'https://unsplash.com/photos/aerial-view-photography-of-forest-A-fubu9QJxE'                // （可选）原作品或艺术家页面的链接
     }
   },
   toc: {
