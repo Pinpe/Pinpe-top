7b70f817a1aaf25d620cafd03e3b92983bf59c6c
Pinpe <813233375@qq.com>
2026-09-26 16:13:47 +0800

    l

--- src/components/links/LinkGridList.vue
+++ src/components/links/LinkGridList.vue
@@ -660,6 +660,12 @@ const links: { title: string; link: string; desc: string; img: string }[] = [
     link: 'https://agent.pinpe.top/',
     img: 'http://localhost:4321/_image?href=%2F%40fs%2Fhome%2Fpinpe%2F%E6%96%87%E6%A1%A3%2F%E4%BB%A3%E7%A0%81%E5%92%8C%E9%A1%B9%E7%9B%AE%2FPinpe-top%2Fsrc%2Fcontent%2Fspec%2Fabout%2Fimages%2Findex%2F%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE_20260709_124124.png%3ForigWidth%3D1920%26origHeight%3D1080%26origFormat%3Dpng&w=1920&h=1080&f=webp',
     desc: '唔嗯，欢迎来到仙狐的小窝～'
+  },
+  {
+    title: 'WenBlog',
+    link: 'https:/wenlei.top/',
+    img: 'https://wenlei.top/wp-content/uploads/2026/07/cropped-wenblog-iOS-Default-1024@1x.webp',
+    desc: 'Debug The World.'
   }
 ];
 
